5407ca736e1a9e6444a892f6608be58d77e86977
gfreitasrosa <gabriel-freitasrosa@hotmail.com>
2023-11-14 01:35:34 -0300

    Ajustes e testes dos metodos avaliativos

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+from metodos_avaliativos import *
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import numpy as np
 
-def abrir_seletor_arquivo():
+def executar_mlp():
     filepath = filedialog.askopenfilename(
         title="Selecione uma imagem",
         filetypes=[("Arquivos de imagem", (".png", ".jpg", ".jpeg", ".gif"))]
     )
     if filepath:
-        exibir_imagem(filepath)
-        converter_para_matriz(filepath)
+        classificar_imagem(mlp_clf, filepath)
 
+def executar_sgd():
+    filepath = filedialog.askopenfilename(
+        title="Selecione uma imagem",
+        filetypes=[("Arquivos de imagem", (".png", ".jpg", ".jpeg", ".gif"))]
+    )
+    if filepath:
+        classificar_imagem(sgd_clf, filepath)
+
+'''
 def exibir_imagem(filepath):
     imagem = Image.open(filepath)
     imagem = imagem.resize((300, 300), Image.ANTIALIAS)
@@ -19,12 +28,7 @@ def exibir_imagem(filepath):
 
     label_imagem.config(image=imagem)
     label_imagem.imagem = imagem
-
-def converter_para_matriz(filepath):
-    imagem = Image.open(filepath)
-    matriz = np.array(imagem)
-    print("Matriz NumPy:")
-    print(matriz)
+'''
 
 #def realiza_teste():
     
@@ -38,10 +42,10 @@ altura = 200
 janela.geometry(f"{largura}x{altura}")
 
 # Botão para abrir o seletor de arquivos
-btn_selecionar = tk.Button(janela, text="Selecionar Imagem", command=abrir_seletor_arquivo)
-btn_teste = tk.Button(janela, text="Executar teste", command="")
-btn_teste.pack(pady=10)
-btn_selecionar.pack(pady=10)
+btn_executar_sgd = tk.Button(janela, text="Executar teste Classificador SGD", command=executar_sgd)
+btn_exceutar_mlp = tk.Button(janela, text="Executar teste Classificador MLP", command=executar_mlp)
+btn_executar_sgd.pack(pady=10)
+btn_exceutar_mlp.pack(pady=10)
 
 # Rótulo para exibir a imagem selecionada
 label_imagem = tk.Label(janela)
